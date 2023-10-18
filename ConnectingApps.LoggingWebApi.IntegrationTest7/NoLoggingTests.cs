3b65431ccd55e3cd6a9166cd41c443a06a88b912
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-18 15:00:12 +0200

    Test is done with .NET 7

---
+++ ConnectingApps.LoggingWebApi.IntegrationTest7/NoLoggingTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ConnectingApps.LoggingWebApi.IntegrationTest7
+{
+    public  class NoLoggingTests : IDisposable
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public NoLoggingTests()
+        {
+            _factory = new WebApplicationFactory<Program>();
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReadInTestOutputIfNothingIsLogged()
+        {
+            var response = await _client.GetAsync("/WeatherForecast");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        public void Dispose()
+        {
+            _factory.Dispose();
+            _client.Dispose();
+        }
+    }
+}
---
+++ ConnectingApps.LoggingWebApi.IntegrationTest7/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ConnectingApps.LoggingWebApi.IntegrationTest7
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

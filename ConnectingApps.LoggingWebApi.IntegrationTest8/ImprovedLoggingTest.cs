40277c84aec85753042e4e35e3cd5e36f3aecad4
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-19 09:23:29 +0000

    Created first test for .NET 8

---
+++ ConnectingApps.LoggingWebApi.IntegrationTest8/ImprovedLoggingTest.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using ConnectingApps.Xunit.TestLogger;
+using Xunit.Abstractions;
+
+namespace ConnectingApps.LoggingWebApi.IntegrationTest7
+{
+    public class ImprovedLoggingTest : IDisposable
+    {
+        private readonly TestLoggerWebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+
+        public ImprovedLoggingTest(ITestOutputHelper output)
+        {
+            _factory = new TestLoggerWebApplicationFactory<Program>(output);
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReadInTestOutputIfSomethingIsLogged()
+        {
+            var response = await _client.GetAsync("/weatherforecast");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        public void Dispose()
+        {
+            _factory.Dispose();
+            _client.Dispose();
+        }
+    }
+}
